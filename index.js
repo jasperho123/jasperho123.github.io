4802978bfdd97a95374afd4be3afc280aecb5353
J <J>
2024-12-12 13:48:08 -0800

    12/12/2024

---
+++ index.js
@@ -0,0 +1,24 @@
+
+
+
+function gameoneoptone() {
+    document.getElementById('answerone').innerText = "Answer = Correct!!!!";
+    document.getElementById('unknownwicked').innerText = "Got $320.5 Million";
+}
+
+function gameoneopttwo() {
+    document.getElementById('answerone').innerText = "Answer = Wrong....";
+    document.getElementById('unknownwicked').innerText = "Got $320.5 Million";
+}
+
+function gametwooptone() {
+    document.getElementById('answertwo').innerText = "Answer = Wrong...."
+    document.getElementById('unknownblinding').innerText = "4,605,005,959(4.6 Billion)"
+}
+
+function gametwoopttwo() {
+    document.getElementById('answertwo').innerText = "Answer = Correct!!!!"
+    document.getElementById('unknownblinding').innerText = "4,605,005,959(4.6 Billion)"
+}
+
+
